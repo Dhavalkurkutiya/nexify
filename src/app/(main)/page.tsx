72997d58ba3ad002e1d7a5abd46b5baf809a3f97
Dhaval Kurkutuiya <Kurkutiyadhaval30@gmail.com>
2024-07-29 09:44:32 +0530

    06:Responsive menubar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <main>
-    Front Page
-  </main>
+    <main className="h-[200vh] w-full bg-red-400">
+      <div className="w-full">
+        Front page
+      </div>
+    </main>
   );
 }
